1fe8030cdc277aed9b353910ae71e7dac0196162
Zachary Shand <zachary.shand@cybera.ca>
2022-10-20 15:14:10 -0600

    Shorten default overlay names to fit in shapefile format

--- scripts/data/create_overlays.py
+++ scripts/data/create_overlays.py
@@ -7,15 +7,17 @@ from src.datasets.overlays import overlay
 
 from datetime import datetime
 
+import warnings
+
 from src.config import DATA_DIRECTORY, OVERLAYS_DIR
 
 DA_OVERLAY_DIR = OVERLAYS_DIR  # DATA_DIRECTORY / 'boundary_overlays'
 DA_OVERLAY_DIR.mkdir(exist_ok=True)
 
 default_files = [
-    ("population_centres", "popctrs"),
+    ("population_centres", "pops"),
     ("dissemination_areas", "das"),
-    ("census_subdivisions", "subdivs"),
+    ("census_subdivisions", "subs"),
     ("census_divisions", "divs"),
 ]
 
@@ -46,6 +48,7 @@ def save_overlay(statcan_boundary, short_name):
         inplace=True,
     )
     # da_tile_overlay["quadkey"] = da_tile_overlay.astype(str)
+    # quadkey field needs to be str to avoid errors related to field width on save to Shapefile
     tile_overlay["quadkey"] = (
         tile_overlay["quadkey"]
         .astype("Int64")
@@ -53,17 +56,20 @@ def save_overlay(statcan_boundary, short_name):
         .apply(lambda s: s if s != "<NA>" else None)
     )
 
-    # tile_overlay.info()
-
-    tile_overlay.to_file(output_shapefile, driver="ESRI Shapefile")
+    with warnings.catch_warnings():
+        warnings.filterwarnings("once")
+        tile_overlay.to_file(output_shapefile, driver="ESRI Shapefile")
     end = datetime.now()
     print(f"Ended at {end}")
     print(f"Elapsed duration {end-start}")
 
-    # da_tile_overlay.info()
 
+# shapefile schema, may be useful to specify if errors persist.
+# {'properties': OrderedDict([('PCUID', 'str:80'), ('PCNAME', 'str:80'), ('PCTYPE', 'str:80'), ('PCPUID', 'str:80'), ('PCCLASS', 'str:80'), ('PRUID', 'str:80'), ('PRNAME', 'str:80'), ('CMAUID', 'str:80'), ('CMANAME', 'str:80'), ('CMATYPE', 'str:80'), ('CMAPUID', 'str:80'), ('popctrs_ar', 'float:24.15'), ('quadkey', 'str:80'), ('tile_area', 'float:24.15'), ('tile_frac', 'float:24.15'), ('popctrs_fr', 'float:24.15')]), 'geometry': 'Polygon'}
 
 if __name__ == "__main__":
+    import warnings
+
     if len(sys.argv) == 1:
         files = default_files
         print("No name, shortname pairs provided as args, using defaults.")
