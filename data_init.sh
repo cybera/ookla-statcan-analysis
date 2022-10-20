f5015365e01f905bb87a1f80794c024a36ed12f8
Zachary Shand <zachary.shand@cybera.ca>
2022-10-20 15:53:59 -0600

    Improve data_init script with more verbose output

--- data_init.sh
+++ data_init.sh
@@ -9,17 +9,41 @@ echo ''
 
 export PYTHONPATH="${PYTHONPATH-.}:./src"
 
-# #
-echo 'Downloading Ookla data from AWS'
-aws s3 sync --no-sign-request --region=us-west-2 s3://ookla-open-data/shapefiles ./data/ookla-raw 
+##
+SECONDS=0
+echo "Downloading Ookla data from AWS"
+now=$(date)
+echo "S3 Download start at  ${now}"
+aws s3 sync --no-sign-request --no-progress --region=us-west-2 s3://ookla-open-data/shapefiles ./data/ookla-raw 
+now=$(date)
+dl_duration=SECONDS
+echo "S3 Download completed at  ${now}"
+duration=dl_duration
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-echo 'Filtering global Ookla tiles to Canada only'
-#conda run -n ookla-statcan scripts/data/process_raw_ookla_faster.py
+##
+SECONDS=0
+now=$(date)
+echo "Filtering to Canda starting at at  ${now}"
+echo "Filtering global Ookla tiles to Canada only"
 python scripts/data/process_raw_ookla_faster.py
+now=$(date)
+echo "Filtering to Canda ended at at ${now}"
+filter_duration=SECONDS
+duration=filter_duration
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
-echo 'Generating complex geometry overlays'
-# conda run -n ookla-statcan scripts/data/create_overlays.py population_centres popctrs 
-# conda run -n ookla-statcan scripts/data/create_overlays.py dissemination_areas das
-python scripts/data/create_overlays.py population_centres popctrs 
+##
+SECONDS=0
+echo "Generating complex geometry overlays"
+python scripts/data/create_overlays.py population_centres pops
 python scripts/data/create_overlays.py dissemination_areas das
+overlay_duration=SECONDS
+overlay_duration=dl_duration
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 
+now=$(date)
+echo "All completed at ${now}"
+
+duration=dl_duration+filter_duration+overlay_duration
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
