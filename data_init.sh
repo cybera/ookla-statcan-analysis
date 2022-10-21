050b288e51f2101aec34fb6c1803da9ee21a55c6
Zachary Shand <zachary.shand@cybera.ca>
2022-10-21 17:41:05 +0000

    Fix total time calc/print in data_init scripts

--- data_init.sh
+++ data_init.sh
@@ -16,10 +16,13 @@ now=$(date)
 echo "S3 Download start at  ${now}"
 aws s3 sync --no-sign-request --no-progress --region=us-west-2 s3://ookla-open-data/shapefiles ./data/ookla-raw 
 now=$(date)
-dl_duration=SECONDS
+dl_duration=$SECONDS
 echo "S3 Download completed at  ${now}"
 duration=dl_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 ##
 SECONDS=0
@@ -29,21 +32,30 @@ echo "Filtering global Ookla tiles to Canada only"
 python scripts/data/process_raw_ookla_faster.py
 now=$(date)
 echo "Filtering to Canda ended at at ${now}"
-filter_duration=SECONDS
+filter_duration=$SECONDS
 duration=filter_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 ##
 SECONDS=0
 echo "Generating complex geometry overlays"
 python scripts/data/create_overlays.py population_centres pops
-python scripts/data/create_overlays.py dissemination_areas das
-overlay_duration=SECONDS
+# python scripts/data/create_overlays.py dissemination_areas das
+overlay_duration=$SECONDS
 duration=overlay_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 now=$(date)
 echo "All completed at ${now}"
 
-duration=dl_duration+filter_duration+overlay_duration
+duration=$(($dl_duration+$filter_duration+$overlay_duration))
+echo "S3 download time  $(($dl_duration / 60)) minutes and $(($dl_duration % 60))"
+echo "Spatial join time $(($filter_duration / 60)) minutes and $(($filter_duration % 60))"
+echo "Overlay time      $(($overlay_duration / 60)) minutes and $(($overlay_duration % 60))"
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
--- data_init_benchmark.sh
+++ data_init_benchmark.sh
@@ -17,10 +17,13 @@ echo "S3 Download start at  ${now}"
 ## restrict shapefiles for download to be faster.
 aws s3 sync --no-sign-request --no-progress --region=us-west-2 --exclude "*" --include "*/type=fixed/year=2019/quarter=[12]/*" s3://ookla-open-data/shapefiles ./data/ookla-raw 
 now=$(date)
-dl_duration=SECONDS
+dl_duration=$SECONDS
 echo "S3 Download completed at  ${now}"
 duration=dl_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 ##
 SECONDS=0
@@ -30,21 +33,30 @@ echo "Filtering global Ookla tiles to Canada only"
 python scripts/data/process_raw_ookla_faster.py
 now=$(date)
 echo "Filtering to Canda ended at at ${now}"
-filter_duration=SECONDS
+filter_duration=$SECONDS
 duration=filter_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 ##
 SECONDS=0
 echo "Generating complex geometry overlays"
 python scripts/data/create_overlays.py population_centres pops
 # python scripts/data/create_overlays.py dissemination_areas das
-overlay_duration=SECONDS
+overlay_duration=$SECONDS
 duration=overlay_duration
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+echo ''
+echo ''
+
 
 now=$(date)
 echo "All completed at ${now}"
 
-duration=dl_duration+filter_duration+overlay_duration
+duration=$(($dl_duration+$filter_duration+$overlay_duration))
+echo "S3 download time  $(($dl_duration / 60)) minutes and $(($dl_duration % 60))"
+echo "Spatial join time $(($filter_duration / 60)) minutes and $(($filter_duration % 60))"
+echo "Overlay time      $(($overlay_duration / 60)) minutes and $(($overlay_duration % 60))"
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
