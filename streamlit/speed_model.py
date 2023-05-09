edd73ad07fa6391583d92962e872c924e6db5d2c
devanshi <devanshi2122002@gmail.com>
2023-05-09 21:40:39 +0000

    added code for data generation in notebook, reference to webapp , instructions to generate in README

--- streamlit/speed_model.py
+++ streamlit/speed_model.py
@@ -15,7 +15,7 @@ This is a space to work and make a new app!
 #### EDA SECTION
 st.write("A")
 
-for_visualization = pd.read_csv("./notebooks/Gap_Analysis.csv")
+for_visualization = pd.read_csv("./data/Gap_Analysis.csv")
 for_visualization["Percentage_gap"] = (for_visualization["Crt_Size_Total"]/for_visualization["Size_Total"])*100
 for_visualization.sort_values(by="Percentage_gap", ascending=False,inplace=True)
 
@@ -27,20 +27,12 @@ fig = px.bar(
     x="Proviences",
     title="Ookla 50/10 GAP by Province",
 )
-# fig.add_hline(
-#     y=canada_stats.loc["Urban", "Percentage_Ookla"],
-#     annotation_text="% Percentage of GAP",
-# )
 fig.update_yaxes(range=[0, 100])
 st.plotly_chart(fig)
 
 st.markdown(
     """
-We can compare the Ookla values, with those data as published by 
-in the National Broadband Map, broken down by province. Generally, 
-the trend is that the Ookla access is lower than the 
-published access levels. A few provinces, like Prince Edward Island 
-or Saskatchewan have substantial differences of 30% or more.
+We are comparing the gap between expected 100% Canadians having >50Mbps Download & >10Mbps uplod speed but the graph show how much % of localities lag the speed expected.
 """
 )
 
