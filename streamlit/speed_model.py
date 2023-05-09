c2cc731b8f35e04ae25d1de9f6de23aa247cf0ce
devanshi <devanshi2122002@gmail.com>
2023-05-09 21:22:23 +0000

    Added code to generate CSV in notebook & refered in webapp

--- streamlit/speed_model.py
+++ streamlit/speed_model.py
@@ -16,7 +16,33 @@ This is a space to work and make a new app!
 st.write("A")
 
 for_visualization = pd.read_csv("./notebooks/Gap_Analysis.csv")
+for_visualization["Percentage_gap"] = (for_visualization["Crt_Size_Total"]/for_visualization["Size_Total"])*100
+for_visualization.sort_values(by="Percentage_gap", ascending=False,inplace=True)
+
 st.write(for_visualization)
+
+fig = px.bar(
+    for_visualization,
+    y="Percentage_gap",
+    x="Proviences",
+    title="Ookla 50/10 GAP by Province",
+)
+# fig.add_hline(
+#     y=canada_stats.loc["Urban", "Percentage_Ookla"],
+#     annotation_text="% Percentage of GAP",
+# )
+fig.update_yaxes(range=[0, 100])
+st.plotly_chart(fig)
+
+st.markdown(
+    """
+We can compare the Ookla values, with those data as published by 
+in the National Broadband Map, broken down by province. Generally, 
+the trend is that the Ookla access is lower than the 
+published access levels. A few provinces, like Prince Edward Island 
+or Saskatchewan have substantial differences of 30% or more.
+"""
+)
 
 
 
