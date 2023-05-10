ba246c221b17c87ea1553ba353d99b7e81ed1e7c
devanshi <devanshi2122002@gmail.com>
2023-05-10 19:36:12 +0000

    Added map visualiztion code to the webapp

--- streamlit/speed_model.py
+++ streamlit/speed_model.py
@@ -3,40 +3,48 @@ import pandas as pd
 
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from src.datasets.loading import statcan, ookla
 
-st.write("# Hello!")
+st.write("# Canada Internet")
+st.write("## JT Take Us To The Promised Land (of 50/10)")
 
-st.write("""
-This is a space to work and make a new app!
-""")
-
-
+st.markdown("<h1 class='custom-header'>1) Exploring the data</h1>", unsafe_allow_html=True)
 
 #### EDA SECTION
-st.write("A")
-
 for_visualization = pd.read_csv("./data/Gap_Analysis.csv")
 for_visualization["Percentage_gap"] = (for_visualization["Crt_Size_Total"]/for_visualization["Size_Total"])*100
 for_visualization.sort_values(by="Percentage_gap", ascending=False,inplace=True)
 
-st.write(for_visualization)
+def bar_graph_for_GAP_analysis(for_visualization):
+    #st.write(for_visualization)
+    fig = px.bar(
+        for_visualization,
+        y="Percentage_gap",
+        x="Proviences",
+        title="Ookla 50/10 GAP by Province",
+    )
+    fig.update_yaxes(range=[0, 100])
+    st.plotly_chart(fig)
 
-fig = px.bar(
-    for_visualization,
-    y="Percentage_gap",
-    x="Proviences",
-    title="Ookla 50/10 GAP by Province",
-)
-fig.update_yaxes(range=[0, 100])
-st.plotly_chart(fig)
-
-st.markdown(
+    st.markdown(
+        """
+    We are comparing the gap between expected 100% Canadians having >50Mbps Download & >10Mbps uplod speed but the graph show how much % of localities lag the speed expected.
     """
-We are comparing the gap between expected 100% Canadians having >50Mbps Download & >10Mbps uplod speed but the graph show how much % of localities lag the speed expected.
-"""
-)
+    )
 
+def generating_map(for_visualization):
+    for_visualization = for_visualization.rename({'Proviences': 'PRNAME'}, axis=1)
+    for_visualization = statcan.boundary('provinces').merge(for_visualization[['PRNAME','Percentage_gap']],on="PRNAME",how='left')
+    t = statcan.boundary('provinces').merge(for_visualization).plot(column='Percentage_gap', legend=True, missing_kwds={'color':'gray'})
+    plt.gcf().suptitle("Gap in each provience for Download/Up speed of 50/10 Mbps")
+    plt.gca().set(xlabel="% Percentage of Gap", ylabel="% Percentage of Gap")
+    #plt.savefig('my_plot.png')
+    return st.pyplot(plt)
 
+#GAP analysis functions
+bar_graph_for_GAP_analysis(for_visualization)
+generating_map(for_visualization)
 
 #### Clustering Section
 st.write("A")
