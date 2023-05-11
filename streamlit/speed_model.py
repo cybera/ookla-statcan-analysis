2ea450b6d5f993e12dd469a6a81c76c762a9adc4
devanshi <devanshi2122002@gmail.com>
2023-05-11 17:09:20 +0000

    Updated the devices visualization

--- streamlit/speed_model.py
+++ streamlit/speed_model.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from src.datasets.loading import statcan, ookla
+import seaborn as sns
 
 st.write("# Canada Internet")
 st.write("## JT Take Us To The Promised Land (of 50/10)")
@@ -34,17 +35,34 @@ def bar_graph_for_GAP_analysis(for_visualization):
     )
 
 def generating_map(for_visualization):
-    for_visualization = for_visualization.rename({'Proviences': 'PRNAME'}, axis=1)
-    for_visualization = statcan.boundary('provinces').merge(for_visualization[['PRNAME','Percentage_gap']],on="PRNAME",how='left')
-    t = statcan.boundary('provinces').merge(for_visualization).plot(column='Percentage_gap', legend=True, missing_kwds={'color':'gray'})
+    for_visualization_map = for_visualization.rename({'Proviences': 'PRNAME'}, axis=1)
+    for_visualization_map = statcan.boundary('provinces').merge(for_visualization_map[['PRNAME','Percentage_gap']],on="PRNAME",how='left')
+    t = statcan.boundary('provinces').merge(for_visualization_map).plot(column='Percentage_gap', legend=True, missing_kwds={'color':'gray'})
     plt.gcf().suptitle("Gap in each provience for Download/Up speed of 50/10 Mbps")
     plt.gca().set(xlabel="% Percentage of Gap", ylabel="% Percentage of Gap")
-    #plt.savefig('my_plot.png')
     return st.pyplot(plt)
+
+def devices_speed_check_alberta(for_visualization_devices):
+    for_visualization_download = for_visualization_devices[for_visualization_devices['PRNAME'] == 'Alberta' ]
+    for_visualization_download = for_visualization_download.pivot_table(values='avg_d_mbps',index='PRNAME',columns='devices')
+    fig, ax = plt.subplots()
+    sns.heatmap(for_visualization_download,annot=True)
+    return st.write(fig)
+
+def devices_speed_check_all_provience(for_visualization_devices):
+    ftr_down_all_provience = for_visualization_devices.pivot_table(values='avg_d_mbps',index='PRNAME',columns='devices')
+    fig, ax = plt.subplots()
+    sns.heatmap(ftr_down_all_provience,annot=True)
+    return st.write(fig)
+    #return st.pyplot(plt,use_container_width=True) 
 
 #GAP analysis functions
 bar_graph_for_GAP_analysis(for_visualization)
 generating_map(for_visualization)
+for_visualization_devices = pd.read_csv("./notebooks/Features.csv")
+devices_speed_check_all_provience(for_visualization_devices)
+devices_speed_check_alberta(for_visualization_devices)
+
 
 #### Clustering Section
 st.write("A")
