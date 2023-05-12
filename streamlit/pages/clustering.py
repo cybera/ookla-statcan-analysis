f49f1cf393a9f191b6ac4d95f5ef4f04044de7c8
Yvonnebun <ywangcal12@gmail.com>
2023-05-12 06:18:42 +0000

    Changing the maps and modify filters and colors.

--- streamlit/pages/clustering.py
+++ streamlit/pages/clustering.py
@@ -8,24 +8,20 @@ import pandas as pd
 import geopandas as gp
 import src.config 
 from src.datasets.loading import statcan
-from notebooks.cluster import preparedata,best_k,AddCluster
+from notebooks.cluster import preparedata,AddCluster,best_k
 from notebooks.clusterTable import import_table
-
+from sklearn.cluster import KMeans
 from OSMPythonTools.api import Api
 from OSMPythonTools.nominatim import Nominatim
 
 import plotly.express as px
 import plotly.graph_objects as go
-
+import matplotlib.pyplot as plt
 from os.path import exists
 
 def subset_province(prname,t):
-    pruid = (
-        t.loc[lambda s: s.PRNAME == prname, "PRUID"].iloc[0]
-    )
-    subset = t.loc[lambda s: s.PRNAME == int(pruid)]
+    subset = t.loc[lambda s: s.PRNAME == prname]
     return subset
-
 if exists("notebooks/cluster_table"):
     features_table=pd.read_pickle("notebooks/cluster_table")
 else:
@@ -34,18 +30,21 @@ else:
 pr_names = list(features_table.loc[:, "PRNAME"].unique())
 pr_names.append("All")
 
-colors=['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black']
+colors=['#2FA49F','#E6A49F','#9C379F','#9C372D','#FF672D','#FF67FF',
+        '#0067FF','#00FAFF','#0046FF','#FF46BD','#F7005A','#96ACFF',
+        '#004E00','#00AD00','#FFD800','#FFB497','#FFFF97','#FF1D97',
+        '#301D02','#93FF02','#91E7FF']
 
 pkey = 'quadkey'
 target_vars = ['avg_d_kbps', 'avg_u_kbps']
-calc_vars=['lat','lon','POP_DENSITY','das_area', 'tile_area', 'tile_frac',  'das_frac']
+calc_vars=['lat','lon','POP_DENSITY','das_area']
 cat_vars=['CSDTYPE','PRNAME','SACTYPE']
 col_subset = [pkey] + calc_vars+target_vars+cat_vars
 
-X=preparedata(calc_vars,cat_vars,target_vars,df=features_table)
-k=best_k(20,X,'k-means++',500,10,42)
-X=AddCluster(k=k,df=X)
-features_table[["cluster","centroids"]] = X[["cluster","centroids"]]
+
+# k=best_k(20,X,'k-means++',500,10,42)
+# X=AddCluster(k=k,df=X)
+#features_table[["cluster","centroids"]] = X[["cluster","centroids"]]
 
 st.markdown("# :blue[🌏Clustering Areas with Similar Features] 🌏")
 
@@ -54,21 +53,43 @@ if option=='All':
     pr_table=features_table
 else:
     pr_table=subset_province(option,features_table)
-
-pr_table['lat']=pr_table.apply(lambda x:float(x['lat']),axis=1)
-pr_table['lon']=pr_table.apply(lambda x:float(x['lon']),axis=1)
-pr_table['cluster']=pr_table.apply(lambda x:int(x['cluster'].astype(int)),axis=1)
-
-m = folium.Map(location=[-79.12078857421875, 43.8206565436291], zoom_start=3, control_scale=True)
-
-#Loop through each row in the dataframe
-for i,row in pr_table.iterrows():
-    iframe = folium.IFrame('Province' + str(row["PRNAME"]))
-    popup = folium.Popup(iframe, min_width=300, max_width=300)
-    icon_color = colors[row['cluster']]
-    folium.Marker(location=[row['lat'],row['lon']],
-                  popup = popup, icon=folium.Icon(color=icon_color, icon='')).add_to(m)
-
-st_data = st_folium(m, width=700) 
+X=preparedata(calc_vars,cat_vars,target_vars,df=pr_table)
+k=best_k(20,X,'k-means++',100,10,42)
+centers=AddCluster(k=k,df=X,original_df=pr_table)
 
 
+colorsmap=zip(range(0,20),colors[:20])
+colorsmap=dict(colorsmap)
+pr_table['colors']=pr_table['cluster'].map(colorsmap)
+
+
+fig = px.scatter_mapbox(pr_table, lat='lon', lon='lat',color='colors',hover_name='cluster',width=950, color_discrete_sequence=px.colors.qualitative.Light24)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
+
+# M = folium.Map(location=[-79.12078857421875, 43.8206565436291], zoom_start=3, control_scale=True)
+# #pr_table.head(20).explore("cluster",k=12,categorical=True,m=M)
+
+
+# #Loop through each row in the dataframe
+# for i,c in pr_table.sample(200).iterrows():
+#     iframe = folium.IFrame('Province' )
+#     popup = folium.Popup(iframe, min_width=300, max_width=300)
+#     icon_color = colors[c['cluster']]
+#     ICON=folium.Icon(color=icon_color, icon='')
+#     folium.CircleMarker(location=[c['lat'],c['lon']],
+#                   popup = popup,icon=ICON).add_to(M)
+
+# st_data = st_folium(M, width=700) 
+
+st.markdown(
+    """
+    From the above graph, we can see that eachj province has dramatically different color distribution. Due to the lack of data points in some areas, 
+    we can only get a partial ituition of which of the areas can be grouped together. For those with sufficient data points, for example, Alberta, 
+    is obviously grouped in to eastern and western parts, which indicates that we might want to consider to apply different strategies to these two 
+    areas. 
+    Similarly, we can conduct analysis on other area's maps, such as 
+
+    """
+)
